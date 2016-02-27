b14f827c9118897829e23263a87ba87e89af8891
oshai <ohadshai@gmail.com>
2016-02-27 22:52:35 +0200

    changed log level to debug

--- src/main/kotlin/com/outbrain/amplify/api/Example.kt
+++ src/main/kotlin/com/outbrain/amplify/api/Example.kt
@@ -1,6 +1,7 @@
 package com.outbrain.amplify.api
 
 fun main(args : Array<String>) {
+    System.setProperty("org.slf4j.simpleLogger.defaultLogLevel","debug")
     val token = "42"
     val api = AmplifyApiClient(token)
     println(api.marketers().id("id").get())
