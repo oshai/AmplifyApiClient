7426015fce79d21596b38c259790e241ec379b4c
oshai <ohadshai@gmail.com>
2016-12-14 00:25:06 +0200

    bump kotlin logging to 1.4.1

--- src/main/kotlin/com/outbrain/amplify/api/Example.kt
+++ src/main/kotlin/com/outbrain/amplify/api/Example.kt
@@ -2,16 +2,16 @@ package com.outbrain.amplify.api
 
 import com.outbrain.amplify.api.data.BlockedPublisher
 import com.outbrain.amplify.api.data.UpdateMarketer
-import mu.KLogger
-import mu.KLogging
+import mu.KotlinLogging
 
-object Log : KLogging()
+val logger = KotlinLogging.logger {}
+
 fun main(args : Array<String>) {
     System.setProperty("org.slf4j.simpleLogger.defaultLogLevel","debug")
-    Log.logger.info { "starting" }
+    logger.info { "starting" }
     val token = "42"
     val api = AmplifyApiClient(token)
     println(api.marketers().id("id_1").get())
     println(api.marketers().id("id_1").update(UpdateMarketer(listOf(BlockedPublisher("1")))))
-    Log.logger.info { "finished" }
+    logger.info { "finished" }
 }
